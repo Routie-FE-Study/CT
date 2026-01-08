2c89718bbd2603a6112badf9139782fccc61dbdf
ohgus <anakin1108@naver.com>
2026-01-08 17:28:56 +0900

    chore(오거스): 모듈 템플릿 추가

---
+++ 오거스/template.js
@@ -0,0 +1,19 @@
+// fs template
+// const fs = require('fs');
+// const input = fs.readFileSync('./오거스').toString().split('\n');
+
+// readline template
+// const rl = require('readline').createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// let rlInput = [];
+// rl.on('line', function(line) {
+    // 콘솔 입력 창에서 줄바꿈(Enter)를 입력할 때마다 호출
+    // input.push(line);
+// }).on('close', function() {
+    // 콘솔 입력 창에서 Ctrl + C 혹은 Ctrl + D를 입력하면 호룿(입력의 종료)
+//     console.log(input);
+//     process.exit();
+// });
