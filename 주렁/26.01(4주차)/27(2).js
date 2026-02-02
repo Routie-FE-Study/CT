0892ed8ab0ace438e161060b93637b56029c55dc
AHHYUNJU <wndkgus0225@naver.com>
2026-02-02 09:26:22 +0900

    feat(주렁): 1월 4주차 2번 풀이

---
+++ 주렁/26.01(4주차)/27(2).js
@@ -0,0 +1,26 @@
+function solution(answers) {
+  const patterns = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+  const scores = [0, 0, 0];
+
+  answers.forEach((answer, i) => {
+    patterns.forEach((pattern, j) => {
+      if (answer === pattern[i % pattern.length]) {
+        scores[j]++;
+      }
+    });
+  });
+
+  const maxScore = Math.max(...scores);
+  const result = [];
+
+  for (let i = 0; i < scores.length; i++) {
+    if (maxScore === scores[i]) {
+      result.push(i + 1);
+    }
+  }
+  return result;
+}
