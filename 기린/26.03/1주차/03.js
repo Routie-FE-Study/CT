591c181e0344069fc3adfb3665699c6dc68d27fd
jeongyou <wjddbwjd1205@naver.com>
2026-03-06 19:05:58 +0900

    feat(기린): 3월 1주차 3번 다른 접근

--- 기린/26.03/1주차/03.js
+++ 기린/26.03/1주차/03.js
@@ -1,3 +1,4 @@
+// 해시 풀이
 function solution(phone_book) {
   var map = new Map();
 
@@ -11,6 +12,19 @@ function solution(phone_book) {
 
       if (map.has(prefix)) return false;
     }
+  }
+
+  return true;
+}
+
+// 정렬 풀이
+function solution(phone_book) {
+  phone_book.sort()
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    const prefix = phone_book[i + 1].slice(0, phone_book[i].length)
+
+    if (prefix === phone_book[i]) return false;
   }
 
   return true;
